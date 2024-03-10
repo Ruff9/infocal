6f2edd9d6a30fdd9ad6cc38176435f0907eee8cf
Rémy Maucourt <remy.maucourt@yahoo.fr>
2024-03-10 19:02:28 +0100

    refactor with Vue.js as SPA for the front

---
+++ server/infocal/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
+
+
+from . import geo_finder
